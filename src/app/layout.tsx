40a8cba0280879c62c013fe00a9460b6897dc39e
user <user@users.noreply.github.com>
2024-12-18 15:09:34 +1100

    new portfolio v1

---
+++ src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Head from 'next/head';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "IT services",
+  description: "it services",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+
+        <h1 className="text-center text-xl">Web Development Process</h1>
+
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">Requirements Gathering</li>
+          <li className="mb-2">Plan / Strategy</li>
+          <li className="mb-2">UX / UI Design</li>
+          <li className="mb-2">Technology Stack Selection</li>
+          <li className="mb-2">Database Design</li>
+          <li className="mb-2">Development</li>
+          <li className="mb-2">Testing</li>
+          <li className="mb-2">CI/CD</li>
+          <li className="mb-2">Refine UX/UI</li>
+          <li className="mb-2">Hosting and Deployment</li>
+          <li className="mb-2">Documentation</li>
+        </ol>
+
+        <h3 className=''>Please contact us regarding other IT services, 
+                    <br/>We love to take on new challenges</h3>
+      </main>
+
+
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="mailto:andrewswork@protonmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/email.svg"
+            alt="Email icon"
+            width={16}
+            height={16}
+          />
+          andrewswork@protonmail.com
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/andrews-work"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to github →
+        </a>
+      </footer>
+
+    </div>
+  );
+}
