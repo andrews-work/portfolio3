49035fc248b795fdae01dfb5a05f7343df9f92b5
user <user@users.noreply.github.com>
2024-12-18 23:25:18 +1100

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,34 +3,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-        <h1 className="text-center text-xl">Web Development Process</h1>
-
-        <ol className="list-inside list-decimal text-sm text-center md:text-left font-[family-name:var(--font-geist-mono)]">
+        <h1 className="text-center text-xl mb-10">Web Development Process</h1>
+        <ol className="list-inside list-decimal text-sm text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">Requirements Gathering</li>
           <li className="mb-2">Plan / Strategy</li>
           <li className="mb-2">UX / UI Design</li>
           <li className="mb-2">Technology Stack Selection</li>
           <li className="mb-2">Database Design</li>
           <li className="mb-2">Development</li>
-          <li className="mb-2">Testing</li>
-          <li className="mb-2">CI/CD</li>
-          <li className="mb-2">Refine UX/UI</li>
+          <li className="mb-2">Testing + Debugging</li>
+          <li className="mb-2">CI / CD</li>
+          <li className="mb-2">Refine UX / UI</li>
           <li className="mb-2">Hosting and Deployment</li>
           <li className="mb-2">Documentation</li>
         </ol>
-
-        <h3 className='text-white'>Please contact us regarding other IT services, 
-                    <br/>We love to take on new challenges</h3>
+        <h3 className="text-center text-white mt-10">
+          Contact us regarding other services,
+          <br />
+          We love to take on new challenges!
+        </h3>
       </main>
-
-
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="mailto:andrewswork@protonmail.com"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -45,7 +42,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/andrews-work"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -59,7 +56,6 @@ export default function Home() {
           Go to github →
         </a>
       </footer>
-
     </div>
   );
 }
